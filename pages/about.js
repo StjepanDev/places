f2eee2e021bd0d281ff217dc07b567d4f2afa779
grunf <grunf69@yahoo.com>
2021-05-23 12:10:38 +0200

    init commit

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Share your awesome places now!</p>
+    </div>
+  );
+}
---
+++ pages/places/index.js
@@ -0,0 +1,7 @@
+export default function PlacesPage() {
+  return (
+    <div>
+      <h1>My Places</h1>
+    </div>
+  );
+}
