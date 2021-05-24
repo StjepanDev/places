5fabbf4bf2a1698362a9b64bca83627a02e554bf
StjepanDev <grunf69@yahoo.com>
2021-05-24 09:04:54 +0200

    evo

---
+++ pages/places/add.js
@@ -0,0 +1,9 @@
+import Layout from '../../components/Layout';
+
+export default function addPlacePage() {
+  return (
+    <Layout title="Add New Place">
+      <h1>Add Place To Be</h1>
+    </Layout>
+  );
+}
